948ce1a1d4ffa437475d865f9eb82256803311c3
YassSoliman <yassersoliman_1@hotmail.com>
2021-01-02 17:00:26 -0500

    Added event listeners

--- Application.php
+++ Application.php
@@ -7,6 +7,11 @@ use yassersoliman\phpmvc\db\DbModel;
 
 class Application
 {
+	const EVENT_BEFORE_REQUEST = 'beforeRequest';
+	const EVENT_AFTER_REQUEST = 'afterRequest';
+
+	protected array $eventListeners = [];
+
 	public static string $ROOT_DIR;
 
 	public string $layout = 'main';
@@ -56,6 +61,7 @@ class Application
 
 	public function run()
 	{
+		$this->triggerEvent(self::EVENT_BEFORE_REQUEST);
 		try {
 			echo $this->router->resolve();	
 		} catch (Exception $e) {
@@ -85,6 +91,19 @@ class Application
 	public static function isGuest()
 	{
 		return !self::$app->user;
+	}
+
+	public function triggerEvent($eventName)
+	{
+		$callbacks = $this->eventListeners[$eventName] ?? [];
+		foreach ($callbacks as $callback) {
+			call_user_func($callback);
+		}
+	}
+
+	public function on($eventName, $callback)
+	{
+		$this->eventListeners[$eventName][] = $callback;
 	}
 }
 
